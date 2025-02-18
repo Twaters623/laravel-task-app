c4950423428eb08e4627f359a03c9489a750fe4e
Trevor Waters <142362719+Twaters623@users.noreply.github.com>
2025-02-18 21:23:22 +0000

    day 1 done

--- taskApp/resources/views/layouts/app.blade.php
+++ taskApp/resources/views/layouts/app.blade.php
@@ -4,12 +4,12 @@
 <meta charset="UTF-8" />
 <meta name = "viewport" content ="width=device-width, inital-scale=1.0"/>
 <title>Laravel Task App</title>
-@vite('resources/css/app.css')
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
     <body class="bg-gray-100">
 <nav class="bg-white shadow p-4">
-<h1 class="text-xl font-bold text-blue-500">My Task App</h1>
+<h1 class="text-xl font-bold">My Task App</h1>
 
 </nav>
 <main class="p-4">
