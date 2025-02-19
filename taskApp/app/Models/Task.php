5b76da46f2eafce5579b5251efb733a690c97ce0
Trevor Waters <142362719+Twaters623@users.noreply.github.com>
2025-02-19 20:49:44 +0000

    idk

--- taskApp/app/Models/Task.php
+++ taskApp/app/Models/Task.php
@@ -1,20 +1,16 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Task extends Model
 {
-    //
-	//
-	protected $fillable = [
-		'task_name',
-		'task_location',
-		'time_complexity',
-		'materials_required',
-		'deadline',
-		'priority',
-		'category',
-	];
+// Which attributes can be mass assigned
+protected $fillable = [
+'task_name',
+'task_location',
+'time_complexity',
+'materials_required',
+'deadline',
+'priority',
+'category',
+];
 }
