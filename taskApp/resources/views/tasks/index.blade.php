0c03a1257bdc1ea385c250e32b688ce1c483b718
Trevor Waters <142362719+Twaters623@users.noreply.github.com>
2025-02-19 20:40:39 +0000

    added trevor text

--- taskApp/resources/views/tasks/index.blade.php
+++ taskApp/resources/views/tasks/index.blade.php
@@ -1,24 +1,21 @@
 <?php
-
 @extends('layouts.app')
 
 @section('content')
-<h1 class='text-2xl font-bold mb-4'>All Tasks</h1>
-
+<h1 class="text-2xl font-bold mb-4">All Tasks</h1>
 <ul>
 @forelse($tasks as $task)
 <li class="mb-2">
-{{$task->task_name}}
+{{ $task->task_name }}
 </li>
 @empty
 <li>No tasks yet.</li>
 @endforelse
 </ul>
-
-	<div class='mt-4'>
-	<a href ="{{route(tasks.create')}}"
-	class = "bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-Create a new Task
+<div class="mt-4">
+<a href="{{ route('tasks.create') }}"
+class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+Create a New Task
 </a>
 </div>
 @endsection
