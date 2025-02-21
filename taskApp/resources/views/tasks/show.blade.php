bb82d92e315516363316af2b320cff7886f627e7
hazelcoffee <142631267+hazelcoffee@users.noreply.github.com>
2025-02-21 20:01:24 +0000

    hello

--- taskApp/resources/views/tasks/show.blade.php
+++ taskApp/resources/views/tasks/show.blade.php
@@ -1,30 +1,42 @@
 @extends('layouts.app')
 @section('content')
+<style>
+    div {
+        border: 3px solid black;
+    }
+    a {
+        border: 2px solid black;
+        padding: 2px;
+    }
+</style>
 <h1 class="text-2xl font-bold text-gray-800 mb-2">Task Details</h1>
+<main class="m-2 flex flex-col text-white  border-4 border-black shadow-md">
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Task Name:</strong> {{ $task->task_name }}
+    </div>
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Location:</strong> {{ $task->task_location }}
+    </div>
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Time Complexity:</strong> {{ $task->time_complexity }}
+    </div>
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Materials Required:</strong> {{ $task->materials_required }}
+    </div>
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Deadline:</strong> {{ $task->deadline }}
+    </div>
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Priority:</strong> {{ $task->priority }}
+    </div>
+    <div class="bg-blue-500 shadow-md hover:bg-sky-700 p-4 border border-black">
+        <strong>Category:</strong> {{ $task->category }}
+    </div>
+</main>
 
-<div class="rounded-lg bg-blue-500 shadow-md mb-2 hover:bg-sky-700">
-<strong>Task Name:</strong> {{ $task->task_name }}
-</div>
-<div class="rounded-lg bg-blue-500 shadow-md mb-2">
-<strong>Location:</strong> {{ $task->task_location }}
-</div>
-<div class="rounded-lg bg-blue-500 shadow-md mb-2">
-<strong>Time Complexity:</strong> {{ $task->time_complexity }}
-</div>
-<div class="rounded-lg bg-blue-500 shadow-md mb-2">
-<strong>Materials Required:</strong> {{ $task->materials_required }}
-</div>
-<div class="rounded-lg bg-blue-500 shadow-md mb-2">
-<strong>Deadline:</strong> {{ $task->deadline }}
-</div>
-<div class="rounded-lg bg-blue-500 shadow-md mb-2">
-<strong>Priority:</strong> {{ $task->priority }}
-</div>
-<div class="rounded-lg bg-blue-500 shadow-md mb-2">
-<strong>Category:</strong> {{ $task->category }}
-</div>
-<div style="margin-top:20px;">
+<section style="margin-top:20px;">
 <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-primary">Edit Task</a>
 <a href="{{ route('tasks.index') }}" class="btn btn-secondary">Back to List</a>
-</div>
+</section>
+
 @endsection
