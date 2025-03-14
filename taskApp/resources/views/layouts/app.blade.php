f57198d8ebc104d01738e1c89b6addf68750dcf6
Trevor Waters <142362719+Twaters623@users.noreply.github.com>
2025-03-14 21:26:40 +0000

    done for project

--- taskApp/resources/views/layouts/app.blade.php
+++ taskApp/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
     </head>
     <body class="bg-gray-100">
-<nav class="bg-white shadow p-4 block">
+<nav class="bg-white shadow p-4 flex margin-auto">
 <h1 class="text-xl font-bold text-purple-600">Taskie:</h1>
 <h1 class="text-l font-bold text-purple-600 ">Where productivity equals simplicity.</h1>
 </nav>
